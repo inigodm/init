8bd060e5722567106a56872b04c2694dd28b0020
inigodm <inigo.delgado@gmail.com>
2025-02-18 23:41:03 +0100

    Added docker config

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -69,7 +69,33 @@ sudo chsh -s /usr/bin/zsh $USER
 echo "IMPORTANT: After finalization you have not configured p10k, you should do it after installation ends running the command:"
 echo "p10k configure"
 read -n 1 -s -r -p ""
-echo "Resuming"
+echo "Resuming..."
 
+echo "Installing docker"
+echo "Uninstalling conflicting packages"
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+echo "Adding repos"
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+echo "Install docker"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Give permissions"
+echo "Adding current user to docker group"
+sudo groupadd docker
+sudo usermod -aG docker $USER
+echo "Restarting docker"
+sudo systemctl restart docker
 
 # Root
